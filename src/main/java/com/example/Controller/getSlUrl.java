c5ecb539a93820777b0d14256d86162e627794e7
Markustak <policy308@gmail.com>
2017-04-06 16:09:38 +0200

    removed default

--- src/main/java/com/example/Controller/getSlUrl.java
+++ src/main/java/com/example/Controller/getSlUrl.java
@@ -10,8 +10,6 @@ public class getSlUrl {
             returnval = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9204&destId=9302&searchForArrival=0";
         }else if(username.equals("Markus")) {
             returnval = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9180&destId=9302&searchForArrival=0";
-        }else{
-            returnval = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9112&destId=9509&searchForArrival=0";
         }
         return returnval;
     }
