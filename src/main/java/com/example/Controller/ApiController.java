ed917ca60b386610ca829d58ad86397790c200c5
danijelacademy <ama09dbu@student.lu.se>
2017-04-06 11:53:26 +0200

    sl-modul

--- src/main/java/com/example/Controller/ApiController.java
+++ src/main/java/com/example/Controller/ApiController.java
@@ -3,6 +3,7 @@ package com.example.Controller;
 
 import com.example.Domain.Quote;
 import com.example.Repository.Repository;
+import com.sun.jersey.json.impl.writer.JsonEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,7 @@ public class ApiController {
     Repository repository;
 
     @GetMapping("/api/quotes")
-    public Quote randomizeQuotes(
-
-    ) {
+    public Quote randomizeQuotes() {
         List<Quote> quotes = repository.getQuotes();
 
         Random rand = new Random();
@@ -34,7 +33,7 @@ public class ApiController {
 
         String slUrl = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9112&destId=9509&searchForArrival=0";
 
-        return slUrl;
+        return JsonEncoder.encode(slUrl);
     }
 
 }
