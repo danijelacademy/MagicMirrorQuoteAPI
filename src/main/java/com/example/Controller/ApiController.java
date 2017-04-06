471c5e5e1c609c46133e78ebf587ee647fed8dc0
Markustak <policy308@gmail.com>
2017-04-06 14:48:34 +0200

    Addedd getter and setter

--- src/main/java/com/example/Controller/ApiController.java
+++ src/main/java/com/example/Controller/ApiController.java
@@ -31,8 +31,8 @@ public class ApiController {
 
 
     @GetMapping("/api/sl/{username}")
-    public BestObject returnSL(@PathVariable String test) {
-        String slUrl =getSlUrl.returnUrl(test);
+    public BestObject returnSL(@PathVariable String username) {
+        String slUrl =getSlUrl.returnUrl(username);
         BestObject bestObject = new BestObject();
         bestObject.url = slUrl;
         return bestObject;
--- src/main/java/com/example/Controller/BestObject.java
+++ src/main/java/com/example/Controller/BestObject.java
@@ -2,5 +2,15 @@ package com.example.Controller;
 
 
 public class BestObject {
+
     String url ;
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
