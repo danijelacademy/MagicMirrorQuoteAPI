9a26a5cd9d0606f7323766e1426dcb7f67137d72
Markustak <policy308@gmail.com>
2017-04-06 14:54:57 +0200

    jag är retarderad

--- src/main/java/com/example/Controller/getSlUrl.java
+++ src/main/java/com/example/Controller/getSlUrl.java
@@ -7,7 +7,7 @@ public class getSlUrl {
         if(username.equals("Matilda")){
             returnval = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9112&destId=9509&searchForArrival=0";
         }else if(username.equals("Robin")){
-            returnval = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9112&destId=9509&searchForArrival=0";
+            returnval = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9204&destId=9509&searchForArrival=0";
         }else
             returnval = "http://api.sl.se/api2/TravelplannerV2/trip.json?key=cf85888d5497444795b1ba969383ebcd&originId=9204&destId=1002&searchForArrival=0";
         return returnval;
